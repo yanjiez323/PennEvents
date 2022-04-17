561613e35f3a3b48006f9365f14121693e1033ec
Yanjie Zhang <yanjiez323@yahoo.com>
2022-04-17 13:19:18 -0400

    twitter base

--- main/urls.py
+++ main/urls.py
@@ -4,4 +4,9 @@ from . import views
 
 urlpatterns = [
     path('', views.main_view, name='main_view'),
+    path('splash/', views.splash_view, name='splash_view'),
+    path('login/', views.login_view, name='login_view'), 
+    path('signup/', views.signup_view, name='signup_view'), 
+    path('logout/', views.logout_view, name='logout_view'),
+    path('delete/', views.delete_view, name='delete_view')
 ]
